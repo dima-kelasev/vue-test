6129136e544a060a0aace31e359203fa9fcba13e
Dima Kelasev <dkelasev@spotnana.com>
2023-03-09 13:06:47 +0300

    add loader

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="hello" id="testApp">
     <h1>{{ msg }}</h1>
-    <input v-model="input" placeholder="Search project" class="search-input" />
+    <div class="inputAndLoading">
+      <div class="middle" v-if="input.length > 0 && input.length < 3">
+        <div class="bar bar1"></div>
+        <div class="bar bar2"></div>
+        <div class="bar bar3"></div>
+        <div class="bar bar4"></div>
+        <div class="bar bar5"></div>
+        <div class="bar bar6"></div>
+        <div class="bar bar7"></div>
+        <div class="bar bar8"></div>
+      </div>
+      <input
+        v-model="input"
+        placeholder="Search project"
+        class="search-input"
+      />
+    </div>
+
     <ul v-if="input.length > 2" class="answer-list">
       <li
         v-for="(todo, idx) in filterList"
@@ -141,5 +158,67 @@ li {
   margin: 0 10px;
   border-bottom: 1px solid gray;
   cursor: pointer;
+}
+
+.inputAndLoading {
+  position: relative;
+  width: 30%;
+  margin: 0 auto;
+}
+
+.middle {
+  top: 58%;
+  right: -2%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+.bar {
+  width: 2px;
+  height: 20px;
+  background: #fff;
+  display: inline-block;
+  transform-origin: bottom center;
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+  /*   box-shadow:5px 10px 20px inset rgba(255,23,25.2); */
+  animation: loader 1.2s linear infinite;
+}
+.bar1 {
+  animation-delay: 0.1s;
+}
+.bar2 {
+  animation-delay: 0.2s;
+}
+.bar3 {
+  animation-delay: 0.3s;
+}
+.bar4 {
+  animation-delay: 0.4s;
+}
+.bar5 {
+  animation-delay: 0.5s;
+}
+.bar6 {
+  animation-delay: 0.6s;
+}
+.bar7 {
+  animation-delay: 0.7s;
+}
+.bar8 {
+  animation-delay: 0.8s;
+}
+
+@keyframes loader {
+  0% {
+    transform: scaleY(0.1);
+  }
+  50% {
+    transform: scaleY(1);
+    background: yellowgreen;
+  }
+  100% {
+    transform: scaleY(0.1);
+    background: transparent;
+  }
 }
 </style>
